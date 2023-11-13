46422d5d4837c61293e67f8b9b296fb5b9380e91
nsa06035(김진수) <84976593+nsa06035@users.noreply.github.com>
2023-11-13 16:44:42 +0900

    Delete jpa_bulletin_board directory

--- jpa_bulletin_board/src/main/java/com/example/jpa_bulletin_board/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jpa_bulletin_board.repository;
-
-import com.example.jpa_bulletin_board.domain.Comment;
-import com.example.jpa_bulletin_board.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
